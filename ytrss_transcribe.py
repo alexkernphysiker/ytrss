9f06f19824538f4bb47d1ee1a3435456bb073df9
Oleksandr <alexrndl@gmail.com>
2026-05-28 22:13:45 +0300

    Update model version in run_gemini function to use gemini-3-flash-preview

--- ytrss_transcribe.py
+++ ytrss_transcribe.py
@@ -250,7 +250,7 @@ def run_gemini(filename, summarize):
     text = ""
     for chunk in filter_subs(srt):
             response = client.models.generate_content(
-                model='gemini-2.5-flash',
+                model='gemini-3-flash-preview',
                 contents=types.Content(
                     parts=[
                         types.Part(text=make_prompt(lang, summarize = summarize)),
